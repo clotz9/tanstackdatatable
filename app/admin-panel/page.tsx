f6846b299e67b9761618916c3c64705a8ceef409
CBS\asyraf <asyraf@ctos.com.my>
2025-04-28 12:16:37 +0800

    use revalidate for fetch data

--- app/admin-panel/page.tsx
+++ app/admin-panel/page.tsx
@@ -1,4 +1,4 @@
-export const dynamic = "force-dynamic";
+export const revalidate = 60; //cache page every 60 seconds
 
 import { User } from "./components/data-table/columns";
 import { filterRoles, filterStatus } from "./utils/data";
@@ -9,10 +9,17 @@ import { ThemeProvider } from "./components/dark-mode-toggle";
 
 //call users from api
 async function getData(): Promise<User[]> {
+  const isBuilding = process.env.NEXT_PHASE === "phase-production-build";
+
+  // checking if we are in build mode
+  if (isBuilding) {
+    console.log("Skipping API call during build...");
+    return [];
+  }
+
   const apiUrl = process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000";
   const res = await fetch(`${apiUrl}/admin-panel/api/users`, {
-    // next: { revalidate: 60 },
-    cache: "no-store",
+    next: { revalidate: 60 }, //cached for 60 seconds
   });
 
   // handle errors if api call fails
